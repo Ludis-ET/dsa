62251e0998f5b6a6e096fcbb486fc85be2fd04c9
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-08 16:23:27 +0300

    1

--- data structures/array/array.py
+++ data structures/array/array.py
@@ -1,2 +1,10 @@
-# creating an array and tranverse it
 from array import *
+
+#1. creating an array and tranverse it
+my_array = array('i',[1,2,3,4,5])
+
+for i in my_array:
+    print(i) # transversing is listing out all the elements
+
+
+2. 
