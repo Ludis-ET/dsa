b4dc5b1fd603db61f31ed5921e10e97f734d2fb8
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-09 13:37:32 +0300

    tup

--- data structures/array/tuples.py
+++ data structures/array/tuples.py
@@ -5,3 +5,11 @@ createtuple = tuple('ludis') # time complexity of o(1)
 print(tup)
 print(onetup)
 print(createtuple)
+
+
+
+tup1 = (1,2,3,4)
+tup2 = (5,6,7,8)
+
+print(tup1 + tup2) # concatinate tuples
+print(tup1 * 4) 
